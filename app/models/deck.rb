c5805e029240e631cea3f4b1ff1b279bb152e944
Garrett Simpson <elgaricimo@gmail.com>
2014-09-04 16:29:23 +0200

    dev env set up

---
+++ app/models/deck.rb
@@ -0,0 +1,23 @@
+# Top 15 Hold'em Starting Hands
+# AA
+# KK
+# QQ
+# AK (suited)
+# JJ
+# 1010
+# AQ (suited)
+# AJ (suited)
+# AK (off suit)
+# KQ (suited)
+# A10 (suited)
+# KJ (suited)
+# AQ (off suit)
+# 99
+# JQ (suited)
+
+class Deck
+  def initialize
+    @array = [1,2,3,4]
+
+  end
+end
